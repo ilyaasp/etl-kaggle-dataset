3966f95b421409db4408666ba6a0a79b752e91ac
ilyaasp <ilyas.hammam17@gmail.com>
2024-07-12 11:57:03 +0700

    add airflow and reorg project tree

---
+++ dags/events_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+# Ensure the parent directory is in the Python path
+# sys.path.insert(0, '/opt/airflow')
+
+# from etl.events.main import main_etl
+
+def_args = {
+    'owner': 'ilyas',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 7, 11),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'vehicle_data_etl_DAG',
+    default_args=def_args,
+    description='ETL of events table',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl_task = BashOperator(
+    task_id='run_events_etl',
+    bash_command="cd /opt/airflow/etl/vehicle_data && python3 main.py",
+    dag=dag,
+)
+
+run_etl_task
--- kaggle_pipeline/utils.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def _read_json_file(file_path):
-    """Load json file as given by file_path"""
-    with open(file_path, "r") as f:
-        return json.load(f)
